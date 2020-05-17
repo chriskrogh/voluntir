e5c78e3d0a0d9ccebb777790207968924027b31c
Christopher Mohammed <christophermohammed11@outlook.com>
2020-05-17 18:35:27 -0400

    right panel

--- client/src/components/panels/Right/index.tsx
+++ client/src/components/panels/Right/index.tsx
@@ -1,30 +1,59 @@
 import React from 'react';
 import /*type*/ { WithStyles, Theme } from '@material-ui/core/styles';
 import { withStyles, createStyles } from '@material-ui/core/styles';
-import Title from 'components/typography/Title';
+import classnames from 'classnames';
+import { Event } from 'types/event';
+import events from 'data/home';
+import ParagraphText from 'components/typography/ParagraphText';
 
 const styles = (theme: Theme) => createStyles({
     panel: {
         display: 'flex',
+        flexDirection: 'column',
         height: '100%',
-        marginTop: 48,
-        width: 180,
         justifyContent: 'center',
-        backgroundColor: theme.palette.secondary.main,
-        borderRadius: theme.spacing(1),
+        width: 180,
         [theme.breakpoints.down('sm')]: {
             width: 120
         },
         [theme.breakpoints.down('xs')]: {
             display: 'none'
-        }
+        },
+        padding: theme.spacing(1),
+        marginTop: 48,
+        backgroundColor: theme.palette.secondary.main,
+        borderRadius: theme.spacing(1),
+    },
+    row: {
+        marginTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1) / 2,
+        borderBottom: `1px solid ${theme.palette.text.secondary}`
+    },
+    firstRow: {
+        marginTop: 0
     }
 });
+
+const getUpcomingEvents = (): Event[] => {
+    return events;
+}
 
 function RightPanel({ classes }: WithStyles<typeof styles>) {
     return (
         <div className={classes.panel}>
-            <Title text="Right Panel" />
+            {getUpcomingEvents().map((event, index) => (
+                <div
+                    key={event._id}
+                    className={
+                        classnames(
+                            classes.row,
+                            (index === 0) ? classes.firstRow : undefined
+                        )
+                    }
+                >
+                    <ParagraphText text={event.title} />
+                </div>
+            ))}
         </div>
     );
 }
