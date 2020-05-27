7ebfb018144890e2c98029cbae5459ad786db049
Christopher Mohammed <christophermohammed11@outlook.com>
2020-05-27 17:01:53 -0400

    eslint

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,21 +1,21 @@
 export enum Routes {
-    AUTH = '/auth',
-    HOME = '/',
-    EXPLORE = '/explore',
-    PROFILE = '/profile',
-    EVENT = '/event',
-    COMMUNITY = '/community'
+  AUTH = '/auth',
+  HOME = '/',
+  EXPLORE = '/explore',
+  PROFILE = '/profile',
+  EVENT = '/event',
+  COMMUNITY = '/community'
 }
 
 export enum Feeds {
-    HOME = 'Home',
-    EXPLORE = 'Explore'
+  HOME = 'Home',
+  EXPLORE = 'Explore'
 }
 
 export enum Panels {
-    HOME = 'Home',
-    EXPLORE = 'Explore',
-    PROFILE = 'Profile',
-    EVENT = 'Event',
-    COMMUNITY = 'Community',
+  HOME = 'Home',
+  EXPLORE = 'Explore',
+  PROFILE = 'Profile',
+  EVENT = 'Event',
+  COMMUNITY = 'Community',
 }
