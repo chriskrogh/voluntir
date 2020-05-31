9f63605cd16b25b6869b9a157cd5b84c350900b6
Christopher Mohammed <christophermohammed11@outlook.com>
2020-05-31 11:38:48 -0400

    community routes wip

--- src/routers/community.ts
+++ src/routers/community.ts
@@ -1,14 +1,19 @@
 import express, { Request, Response } from 'express';
 import Community, { CommunityDoc } from "../models/community";
+import { UserRequest } from '../types/network';
+import auth from '../middleware/auth';
 import * as M from '../utils/errorMessages';
 import { Routes } from '../utils/constants';
 import '../db/mongoose';
 
 const router = express.Router();
 
-router.post(Routes.COMMUNITY, async (req: Request, res: Response) => {
+router.post(Routes.COMMUNITY, auth, async (req: UserRequest, res: Response) => {
   try {
-    const community = new Community(req.body);
+    const community = new Community({
+      ...req.body,
+      admins: [req.user?._id]
+    });
     res.status(201).send({ community });
   } catch (err) {
     console.log(err);
