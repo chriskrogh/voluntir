0140c7b19e5390fde44f86ad9a1a27b7053852a6
Christopher Mohammed <christophermohammed11@outlook.com>
2020-06-03 20:14:53 -0400

    eslint

--- client/src/types/event.ts
+++ client/src/types/event.ts
@@ -2,15 +2,15 @@ import { ModelMetadata } from './model';
 import { Media } from './media';
 
 export type EventData = {
-    title: string;
-    description: string;
-    media?: Media[];
-    location: string;
-    start: Date;
-    end: Date;
+  title: string;
+  description: string;
+  media?: Media[];
+  location: string;
+  start: Date;
+  end: Date;
 
-    community: string;
-    communityName: string;
+  community: string;
+  communityName: string;
 };
 
 export type Event = EventData & ModelMetadata;
