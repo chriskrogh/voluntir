a4b1144d5680c89e28e93cfe7ebb01fe3cb62b93
Christopher Krogh <chris.krogh@outlook.com>
2020-06-20 08:31:57 -0400

    shell of explore reqs

--- src/routers/event/explore/index.ts
+++ src/routers/event/explore/index.ts
@@ -5,7 +5,6 @@ import express, { Response } from 'express';
 import Event from "../../../models/event";
 import auth from '../../../middleware/auth';
 import { paginateAggregate } from '../utils';
-import { getJoinedCommunityIds, queryAggregate } from './utils';
 import * as M from '../../../utils/errorMessages';
 
 const router = express.Router();
@@ -20,9 +19,7 @@ router.get('/new', auth, async (req: AuthenticatedRequest, res: Response) => {
     if(page == null || isNaN(page)) {
       throw new RouteError(new Error(M.MISSING_PAGE));
     }
-    const joinedCommunityIds = await getJoinedCommunityIds(user);
     const events = await Event.aggregate([
-      ...queryAggregate(joinedCommunityIds, user),
       {
         $sort: { createdAt: -1 }
       },
@@ -45,9 +42,7 @@ router.get('/upcoming', auth, async (req: AuthenticatedRequest, res: Response) =
     if(page == null || isNaN(page)) {
       throw new RouteError(new Error(M.MISSING_PAGE));
     }
-    const joinedCommunityIds = await getJoinedCommunityIds(user);
     const events = await Event.aggregate([
-      ...queryAggregate(joinedCommunityIds, user),
       {
         $match: {
           start: { $gte: new Date() }
@@ -75,9 +70,7 @@ router.get('/completed', auth, async (req: AuthenticatedRequest, res: Response) 
     if(page == null || isNaN(page)) {
       throw new RouteError(new Error(M.MISSING_PAGE));
     }
-    const joinedCommunityIds = await getJoinedCommunityIds(user);
     const events = await Event.aggregate([
-      ...queryAggregate(joinedCommunityIds, user),
       {
         $match: {
           start: { $lt: new Date() }
