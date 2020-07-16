0f05b6981cf75dce7848244d499abee5ee6816d9
Christopher Krogh <chris.krogh@outlook.com>
2020-07-15 22:35:26 -0400

    added dropzone

---
+++ client/src/components/DropZone/index.tsx
@@ -0,0 +1,16 @@
+import /*type*/ { WithStyles, Theme } from '@material-ui/core/styles';
+
+import React from 'react';
+import { withStyles, createStyles } from '@material-ui/core/styles';
+import { useDropzone } from 'react-dropzone';
+import ImageSearchIcon from '@material-ui/icons/ImageSearch';
+
+function DropZone() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default DropZone;
